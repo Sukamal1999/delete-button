f3bde4e8ccd3ac4fb795d0ed6f208596abec8bcb
Sukamal Mondal <sukamalmondal07@gmail.com>
2023-08-16 10:20:05 +0530

    first commit

---
+++ models/cart.js
@@ -0,0 +1,81 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'cart.json'
+);
+
+module.exports = class Cart {
+  static addProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+      const existingProductIndex = cart.products.findIndex(
+        prod => prod.id === id
+      );
+      const existingProduct = cart.products[existingProductIndex];
+      let updatedProduct;
+
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products[existingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products.push(updatedProduct);
+      }
+      cart.totalPrice = cart.totalPrice + +productPrice;
+
+      fs.writeFile(p, JSON.stringify(cart), err => {
+        if (err) {
+          console.error('Error writing cart file:', err);
+        }
+      });
+    });
+  }
+
+  static deleteProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        console.error('Error reading cart file:', err);
+        return;
+      }
+      const updatedCart = { ...JSON.parse(fileContent) };
+      const product = updatedCart.products.find(prod => prod.id === id);
+      
+      if (!product) {
+        console.log(`Product with ID ${id} not found in cart.`);
+        return;
+      }
+      
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(
+        prod => prod.id !== id
+      );
+      updatedCart.totalPrice =
+        updatedCart.totalPrice - productPrice * productQty;
+
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
+        if (err) {
+          console.error('Error writing cart file:', err);
+        }
+      });
+    });
+  }
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        console.error('Error reading cart file:', err);
+        cb(null);
+      } else {
+        const cart = JSON.parse(fileContent);
+        cb(cart);
+      }
+    });
+  }
+};
